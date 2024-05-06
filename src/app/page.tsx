d4d273e4cbd65a40d5d28c6f809a67f3fa82c77a
Agustín Escudero Web <124199392+agustinescuderoweb@users.noreply.github.com>
2024-05-05 23:02:42 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
   return (
     <Layout>
       {loading ? (
-        <p>Loading...</p>
+        <p className={styles.loading}>Loading...</p>
       ) : (
         <>
         <head>
